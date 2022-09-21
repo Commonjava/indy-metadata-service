23cc1ffee513830c0b2710c2e2183b75d4aad17a
Wenjie Guo <wguo@redhat.com>
2022-09-21 13:35:58 +0800

    Update the trace approach

---
+++ src/main/java/org/commonjava/service/metadata/handler/Traced.java
@@ -0,0 +1,12 @@
+package org.commonjava.service.metadata.handler;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.*;
+
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Inherited
+public @interface Traced
+{
+}
