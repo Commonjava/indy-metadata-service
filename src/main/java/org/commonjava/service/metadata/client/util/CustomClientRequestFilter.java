69377731bdf0b59fc14e0958b82d2daafd020b6b
Wenjie Guo <wguo@redhat.com>
2022-11-10 16:10:22 +0800

    Add clientRequestFilter to set token

---
+++ src/main/java/org/commonjava/service/metadata/client/util/CustomClientRequestFilter.java
@@ -0,0 +1,26 @@
+package org.commonjava.service.metadata.client.util;
+
+import io.quarkus.oidc.client.OidcClient;
+
+import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class CustomClientRequestFilter implements ClientRequestFilter
+{
+
+    @Inject
+    OidcClient client;
+
+    @Override
+    public void filter( ClientRequestContext requestContext )
+    {
+        requestContext.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getTokens().await().indefinitely().getAccessToken());
+    }
+}
