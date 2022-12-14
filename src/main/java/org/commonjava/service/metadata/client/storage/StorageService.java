10d7689bfebb2f0fadd1793c5f908029b18ff9ed
Wenjie Guo <wguo@redhat.com>
2022-12-14 13:31:27 +0800

    Update to call the batch cleanup api

--- src/main/java/org/commonjava/service/metadata/client/storage/StorageService.java
+++ src/main/java/org/commonjava/service/metadata/client/storage/StorageService.java
@@ -1,8 +1,10 @@
 package org.commonjava.service.metadata.client.storage;
 
+import org.commonjava.event.storage.BatchCleanupRequest;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -14,5 +16,9 @@ public interface StorageService
     @DELETE
     @Path("/content/{filesystem}/{path: (.*)}")
     Response delete(final @PathParam( "filesystem" ) String filesytem, final @PathParam( "path" ) String path);
+
+    @POST
+    @Path("/filesystem/cleanup")
+    Response cleanup(final BatchCleanupRequest request );
 
 }
