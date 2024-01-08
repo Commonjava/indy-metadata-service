d9e773855048a1e2340d8347d5d9f89e6a90a317
Wenjie Guo <wguo@redhat.com>
2024-01-08 10:09:39 +0800

    Update deps

--- src/test/java/org/commonjava/service/metadata/handler/MockableRepositoryService.java
+++ src/test/java/org/commonjava/service/metadata/handler/MockableRepositoryService.java
@@ -21,13 +21,11 @@ import org.commonjava.service.metadata.client.repository.ArtifactStore;
 import org.commonjava.service.metadata.client.repository.RepositoryService;
 import org.commonjava.service.metadata.client.repository.StoreListingDTO;
 import org.commonjava.service.metadata.model.StoreKey;
-import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Response;
 import java.util.Arrays;
 import java.util.List;
